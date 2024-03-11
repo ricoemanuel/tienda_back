efa5681b317b0f2895171e858efec7ef632a4654
ricoemanuel <ricoruizemanuel@gmail.com>
2024-03-11 15:33:47 -0500

    db

--- apitienda/settings.py
+++ apitienda/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,18 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6p$j9k2nsfb(ec@7!nv$9vsl#00&ba2v72u_f@r)_1j1v7_*f'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+DEBUG = 'RENDER' not in os.environ
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = []
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-# ]
 
-# Application definition
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
